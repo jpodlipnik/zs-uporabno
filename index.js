7305d9b78986a91dfb2e8554ad4c274ebac2c668
jpodlipnik <142104703+jpodlipnik@users.noreply.github.com>
2023-11-28 09:10:20 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -14,3 +14,41 @@ function toggleSidebar() {
 
 menuButton.addEventListener("click", toggleSidebar);
 closeButton.addEventListener("click", toggleSidebar);
+
+function refreshImages() {
+  const gridItems = document.querySelectorAll('.grid-item');
+
+  gridItems.forEach(gridItem => {
+      const images = gridItem.querySelectorAll('img');
+
+      images.forEach(image => {
+          // Generate a random query parameter to prevent browser caching
+          const randomQueryParam = Math.random().toString(36).substring(7);
+          const src = image.getAttribute('src');
+          // Append the random query parameter to the image source to force a refresh
+          image.setAttribute('src', src + '?' + randomQueryParam);
+      });
+  });
+}
+
+// Refresh the images initially
+refreshImages();
+
+// Set up the interval to refresh images every 5 minutes
+setInterval(refreshImages, 5* 10 * 1000); // 5 minutes in milliseconds
+
+function updateTimestamp() {
+  const timestampElement = document.getElementById('refresh-date');
+  const currentTime = new Date();
+  const hours = currentTime.getHours().toString().padStart(2, '0');
+  const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+  const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+
+  const formattedTime = hours + ':' + minutes;
+
+
+  timestampElement.textContent = ' ' + formattedTime;
+}
+
+updateTimestamp();
+setInterval(updateTimestamp, 5 * 60 * 1000);
